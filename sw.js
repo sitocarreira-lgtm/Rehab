28dbb959c8dca942f21dc83920d872135a45b50c
sitocarreira-lgtm <sitocarreira@gmail.com>
2026-08-03 21:30:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service worker · rehab-v4
    Cambia SIEMPRE el número de CACHE al subir una versión nueva:
    es lo que obliga al service worker instalado a renovarse. */
-const CACHE = 'rehab-v6';
+const CACHE = 'rehab-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
